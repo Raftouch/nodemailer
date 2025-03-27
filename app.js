d0561eb0c76c19b12a13119c222a70eb883b0632
raftouch <ntimorshina@gmail.com>
2025-03-27 22:42:27 +0100

    test with array of emails, accepted & rejected

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const html = `
     <p>Today is a beautiful day</p>
 `;
 
+const emails = ["test_2@test.com", "test_3@test.com", "test_4@test.com"];
+
 const start = async () => {
   const transporter = nodeMailer.createTransport({
     service: "gmail",
@@ -17,11 +19,13 @@ const start = async () => {
   try {
     const info = await transporter.sendMail({
       from: "TestUser <test@test.com>",
-      to: "test_2@test.com",
+      to: emails,
       subject: "Testing test",
       html: html,
     });
     console.log("Message sent : ", info.messageId);
+    console.log("Message sent : ", info.accepted);
+    console.log("Message sent : ", info.rejected);
   } catch (error) {
     console.log("Error sending email: ", error);
   }
